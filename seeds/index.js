37ad2bb39d77fa2e2371e9b8218087cf85be1525
VarshathTerli <87375848+VarshathTerli@users.noreply.github.com>
2021-11-24 22:38:32 +0530

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors, collection_ids } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+
+mongoose.connect('mongodb://localhost:27017/My_yelpcamp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const id = collection_ids[i];
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '619e06a7de60e85657dfcd92',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            //image: 'https://source.unsplash.com/collection/483251',
+            image: 'https://source.unsplash.com/collection/' + id.toString(),
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
